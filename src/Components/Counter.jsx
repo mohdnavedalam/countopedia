e99919bea6a4396736a231e21206fb28109a0b73
Mohd Naved Alam <mohd.alam@cae.com>
2023-05-17 10:26:08 +0530

    lecture 55 - state management basics in class based components

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
 class Counter extends React.Component {
-    render () {
-        return (
-            <p>Counter</p>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+        count: 0
     }
+  }
+
+  handleOffence() {
+    alert("attack clicked");
+  }
+  handleDefence() {
+    alert("defence clicked");
+  }
+  render() {
+    return (
+      <div className="row">
+        <h1>Counter: {this.state.cou}</h1>
+        <button onClick={this.handleOffence} className="width-200px">
+          +1
+        </button>
+        <button onClick={this.handleDefence} className="width-200px">
+          -1
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
