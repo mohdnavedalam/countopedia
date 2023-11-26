f899583d7eb135d813085d221667c362f387f0de
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2023-11-26 14:31:00 +0530

    modified Counter.jsx

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -9,6 +9,7 @@ class Counter extends React.Component {
       count: 0,
       lastPlay: "",
       gameStatus: "",
+      gameStatusPrevious: ""
     };
   }
 
@@ -18,8 +19,8 @@ class Counter extends React.Component {
       return {
         count: newCount,
         lastPlay: "Offence",
-        //gameStatus: newCount < 10 || newCount > -10 ? "" : "",
-        gameStatus: newCount > 10 ? "You Won !" : previousState.gameStatus,
+        gameStatus: newCount > 10 ? "You Won !" : "",
+        //gameStatusPrevious: newCount <= 10 ? previousState.gameStatusPrevious : previousState.gameStatusPrevious,
       };
     });
   }
@@ -29,8 +30,8 @@ class Counter extends React.Component {
       return {
         count: newCount,
         lastPlay: "Defence",
-        //gameStatus: newCount < 10 || newCount > -10 ? "" : "",
-        gameStatus: newCount < -10 ? "You Lose !" : previousState.gameStatus,
+        gameStatus: newCount < -10 ? "You Lose !" : "",
+        //gameStatusPrevious : newCount >= -10 ? previousState.gameStatusPrevious : this.state.gameStatus,
       };
     });
   }
@@ -60,6 +61,7 @@ class Counter extends React.Component {
         <p>You win at +10 points and lose at -10 points!</p>
         <p>Last Play: {this.state.lastPlay}</p>
         <h3>Game Status: {this.state.gameStatus}</h3>
+        <h3>Previous status: {this.state.gameStatusPrevious}</h3>
         <div className="col-6 col-md-3 offset-md-3">
           <img
             className="image-styles image-border-green p-4 rounded"
